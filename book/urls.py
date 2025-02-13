a162ba4e2b7ccc135296fd751b2dad41db6c0f05
“Norboyev <samariddin.grex@gmail.com>
2025-02-13 16:36:56 +0500

    almost ready

--- book/urls.py
+++ book/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('delete_book/<int:pk>', views.delete_book, name='delete_book'),
     path('edit_book/<int:pk>', views.edit_book, name='edit_book'),
     path('see_book/<int:pk>', views.see_book, name='see_book'),
+
+
+
+    path('authors/', views.authors, name='authors'),
+    path('create-author/', views.create_author, name='create-author'),
+    path('delete_author/<int:pk>', views.delete_author, name='delete_author'),
+    path('edit_author/<int:pk>', views.edit_author, name='edit_author'),
+    path('see_author/<int:pk>', views.see_author, name='see_author'),
 ]
--- book/views.py
+++ book/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 from .models import Author, Book
+from datetime import datetime
 # Create your views here.
 def home(request):
     books = Book.objects.all()
@@ -30,8 +31,9 @@ def edit_book(request, pk):
             book.author = author
             book.save()
         return redirect(reverse('book:home'))
+    authors = Author.objects.all()
     book = Book.objects.get(id = pk)
-    return render(request, 'book/edit_book.html', {'book':book})
+    return render(request, 'book/edit_book.html', {'book':book, 'authors': authors})
 
 def see_book(request, pk):
     book = Book.objects.get(id = pk)
@@ -50,3 +52,51 @@ def create_book(request):
         return redirect(reverse('book:home'))
     authors = Author.objects.all()
     return render(request, 'book/create_book.html', {'authors': authors})
+
+
+
+
+
+def authors(request):
+    authors = Author.objects.all()
+    books = Book.objects.all()
+    return render(request, 'book/authors.html', {'authors':authors, 'books':books})
+def delete_author(request, pk):
+    if pk:
+        author = Author.objects.get(id = pk)
+        author.delete()
+    return redirect(reverse('book:authors'))
+
+def edit_author(request, pk):
+    if request.method == 'POST':
+        if pk:
+            author = Author.objects.get(id = pk)
+            name = request.POST.get('name')
+            if name: author.name = name
+            surname = request.POST.get('surname')
+            if surname: author.surname = surname
+            birthday = request.POST.get('birthday')
+            birthday = datetime.strptime(birthday, "%Y-%m-%dT%H:%M").strftime("%Y-%m-%d")
+            author.name = name
+            author.surname = surname
+            author.birthday = birthday
+            author.save()
+        return redirect(reverse('book:authors'))
+    author = Author.objects.get(id = pk)
+    author.birthday = author.birthday.strftime("%Y-%m-%dT%H:%M")
+    return render(request, 'book/edit_author.html', {'author':author})
+
+def see_author(request, pk):
+    books = Book.objects.all()
+    author = Author.objects.get(id = pk)
+    return render(request, 'book/see_author.html', {'author':author, 'books':books})
+
+def create_author(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        birthday = request.POST.get('birthday')
+        birthday = datetime.strptime(birthday, "%Y-%m-%dT%H:%M").strftime("%Y-%m-%d")
+        Author.objects.create(name = name, surname = surname, birthday = birthday)
+        return redirect(reverse('book:authors'))
+    return render(request, 'book/create_author.html')
